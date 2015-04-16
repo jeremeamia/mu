8911f116562f12524c5c84520bb5cc69bd3b7326
Jeremy Lindblom <jeremeamia@gmail.com>
2015-04-16 07:48:47 -0700

    Fixed the DIC to properly cache values generated by callable entries.

--- mu.php
+++ mu.php
@@ -1,4 +1,4 @@
-<?php class µ{function cfg($k,$v=null){$c=&$this->$k;return$v!==null&&($c=$v)?$this:($c?(is_callable($v=$c)?$v($this):$v
-):null);}function __call($m,$a){$this->{($m=='any'?'(.*)':strtoupper($m))."#$a[0]"}=$a[1];return$this;}function run(){
+<?php class µ{function cfg($k,$v=null){$c=&$this->$k;if($v===null)return is_callable($c)?($c=$c($this)):$c;$c=$v;return
+$this;}function __call($m,$a){$this->{($m=='any'?'(.*)':strtoupper($m))."#$a[0]"}=$a[1];return$this;}function run(){
 foreach($this as$x=>$f)if(preg_match("@$x@","$_SERVER[REQUEST_METHOD]#$_SERVER[REQUEST_URI]",$p))return$f($this,$p);}
 function view($f,$d=[]){ob_start();extract($d);require$this->cfg('views')."/$f.php";return ob_get_clean();}}#@jeremeamia
