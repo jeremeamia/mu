ec59d77ee7938238334bafc1cc7ec092cd94ec14
Lury <lury@critical.pl>
2015-04-21 07:34:22 +0200

    god dammit can't interpolate xor -.-

--- mu.php
+++ mu.php
@@ -1,4 +1,4 @@
 <?php class µ{function cfg($k,$v=null){$c=&$this->$k;if($v===null)return is_callable($c)?($c=$c($this)):$c;$c=$v;return
 $this;}function __call($m,$a){$this->{($m=='any'?'(.*)':strtoupper($m))."#$a[0]"}=$a[1];return$this;}function run(){
-foreach($this as$x=>$f)if(preg_match(~ø€áø,~€†¨∫≠©∫≠§≠∫Æ™∫¨´†≤∫´∑∞ª¢‹€†¨∫≠©∫≠§≠∫Æ™∫¨´†™≠∂¢,$p))return$f($this,$p);}
-function view($f,$d=[]){ob_start();extract($d);require$this->cfg('views')."/$f.php";return ob_get_clean();}}#@jeremeamia
+foreach($this as$x=>$f)if(preg_match("@$x@","${~ ¬º­©º­¤­º®ªº¬« ²º«·°»¢}#${~¬º­©º­¤­º®ªº¬« ª­¶¢}",$p))return$f($this,$p);}
+function ʋ($f,$d=[]){ob_start();extract($d);require$this->cfg('Ʋ')."/$f.php";return ob_get_clean();}}#@jeremeamia
