55df249b7257e5834314546b9e958dc44ec22946
Spencer Rinehart <anubis@overthemonkey.com>
2015-04-21 20:35:38 -0500

    Remove separator between METHOD and URI.
    The separator wasn't really needed, unless trying to protect against
    some weird and invalid request methods.

--- mu.php
+++ mu.php
@@ -1,4 +1,4 @@
 <?php class µ{function cfg($k,$v=null){$c=&$this->$k;if($v===null)return is_callable($c)?$c=$c($this):$c;$c=$v;return
-$this;}function __call($m,$a){$this->{($m=='any'?'':$m)."#$a[0]"}=$a[1];return$this;}function run(){foreach($this as
-$x=>$f)if(preg_match("@$x@i","$_SERVER[REQUEST_METHOD]#$_SERVER[REQUEST_URI]",$p))return$f($this,$p);}function view($f,
+$this;}function __call($m,$a){$this->{($m=='any'?'':$m).$a[0]}=$a[1];return$this;}function run(){foreach($this as
+$x=>$f)if(preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p))return$f($this,$p);}function view($f,
 $d=[]){ob_start();extract($d);require"$this->views/$f.php";return ob_get_clean();}}#@jeremeamia
