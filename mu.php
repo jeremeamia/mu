40e0d7f7ed6f846d85cf7ff3f13fb091d8d1ed29
Ashley Hutson <asheliahut@gmail.com>
2023-03-21 00:54:46 -0700

    Removes creation of dynamic properties.
    Fix for PHP 8.2 deprecations 
    
    fixes #9

--- mu.php
+++ mu.php
@@ -1,3 +1,3 @@
-<?php class µ{function __call($m,$a){$c=&$this->{$m.$a[0]};$c=$a[1]??(is_callable($c)?$c($this):$c);return isset($a[1])?
-$this:$c;}function run(){foreach($this as$x=>$f)preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p)&&
-$f($this,$p);}function view($f,$d=[]){ob_start();extract($d);require"$this->cfgviews/$f.php";return ob_get_clean();}}#JL
+<?php class µ{private $d=[];function __call($m,$a){$k=$m.$a[0];$c=&$this->d[$k];$c=$a[1]??(is_callable($c)?$c($this):$c);return isset($a[1])?
+$this:$c;}function run(){foreach($this->d as$x=>$f){preg_match("@$x@i","{$_SERVER['REQUEST_METHOD']}{$_SERVER['REQUEST_URI']}",$p)&&
+$f($this,$p);}}function view($f,$d=[]){ob_start();extract($d);require"{$this->d['cfgviews']}/$f.php";return ob_get_clean();}}
