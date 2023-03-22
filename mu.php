60ac7cd4e66e8bc87b41c1079b80e644fef2a8e2
Jeremy Lindblom <jeremeamia@gmail.com>
2023-03-22 14:36:41 -0700

    Update mu.php

--- mu.php
+++ mu.php
@@ -1,3 +1,3 @@
 <?php class µ{var$Δ;function __call($m,$a){$c=&$this->Δ[$m.$a[0]];$c=$a[1]??(is_callable($c)?$c($this):$c);return($a[1]
-??0)?$this:$c;}function view($f,$d=[]){ob_start();extract($d);require"$f.php";return ob_get_clean();}function run(){#<3
-foreach($this->Δ as$x=>$f)preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p)&&$f($this,$p);}}#ByJCL
+??0)?$this:$c;}function view($d,$f,$s=[]){ob_start();extract($s);require"$d/$f.php";return ob_get_clean();}function run
+(){foreach($this->Δ as$x=>$f)preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p)&&$f($this,$p);}}#JL
