1b25ec42b77d1e6c8e8daefffbdebced17e4decf
Jeremy Lindblom <jeremeamia@gmail.com>
2023-03-22 14:27:22 -0700

    Update for 8.2 compatibility

--- mu.php
+++ mu.php
@@ -1,3 +1,3 @@
-<?php class µ{function __call($m,$a){$c=&$this->{$m.$a[0]};$c=$a[1]??(is_callable($c)?$c($this):$c);return isset($a[1])?
-$this:$c;}function run(){foreach($this as$x=>$f)preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p)&&
-$f($this,$p);}function view($f,$d=[]){ob_start();extract($d);require"$this->cfgviews/$f.php";return ob_get_clean();}}#JL
+<?php class µ{var$Δ;function __call($m,$a){$c=&$this->Δ[$m.$a[0]];$c=$a[1]??(is_callable($c)?$c($this):$c);return($a[1]
+??0)?$this:$c;}function view($f,$d=[]){ob_start();extract($d);require"$f.php";return ob_get_clean();}function run(){#<3
+foreach($this->Δ as$x=>$f)preg_match("@$x@i","$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]",$p)&&$f($this,$p);}}#ByJCL
